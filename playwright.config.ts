51bd22b3b1092095d6986bd74bd57b75dd727121
Wojciech Jakub Ciok <wj@halfspace.io>
2023-07-31 12:57:14 +0200

    one workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -24,7 +24,10 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://127.0.0.1:3000',
+    baseURL:
+      process.env.NODE_ENV == "test"
+        ? "https://mango-beach-0a1514d03-test.westeurope.3.azurestaticapps.net/"
+        : "http://localhost:3000",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,14 +1,14 @@
 import { expect, test } from "@playwright/test";
 
 test("has title", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Pokemon Search/);
 });
 
 test("finds pikachu", async ({ page }) => {
-  await page.goto("http://localhost:3000/");
+  await page.goto("/");
 
   await page.getByRole("textbox").fill("pikachu");
 
